a4cae395f465aa2542b86e8e7b70209e8852b395
amaym0n <loco.skitls@gmail.com>
2022-11-27 00:05:44 +0300

    Added new class Hasher in new module hashing.py that will hash user passwords and verify it

---
+++ backend/core/hashing.py
@@ -0,0 +1,16 @@
+from typing import Union
+
+from passlib.context import CryptContext
+
+pwt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class Hasher:
+    @staticmethod
+    def verify_password(plain_password: Union[str, bytes], hashed_password: Union[str, bytes]) -> bool:
+        return pwt_context.verify(secret=plain_password, hash=hashed_password)
+
+    @staticmethod
+    def get_password_hash(plain_password: Union[str, bytes]) -> str:
+        return pwt_context.hash(secret=plain_password)
+
