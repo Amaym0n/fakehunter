ddb7a0a86fbadd5d69ab70d1e11b7ccbc087720f
amaym0n <loco.skitls@gmail.com>
2022-11-26 23:23:57 +0300

    Created user table and User class, did some fixes in jobs.py

--- backend/db/models/jobs.py
+++ backend/db/models/jobs.py
@@ -11,6 +11,6 @@ class Job(Base):
     location = Column(String, nullable=False)
     descriotion = Column(String, nullable=False)
     date_posted = Column(Date)
-    is_active = Column(Boolean, default=True)
+    is_active = Column(Boolean(), default=True)
     owner_id = Column(Integer, ForeignKey('user.id'))
     owner = relationship('User', back_populates='jobs')
