55d739005cfc3e11362a70a6104bab08186f4d58
amaym0n <loco.skitls@gmail.com>
2022-11-26 23:06:00 +0300

    Completed connection to postgres DB using SQLAlchemy

--- backend/db/base_class.py
+++ backend/db/base_class.py
@@ -2,10 +2,11 @@ from typing import Any
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
-@as_declarative
+@as_declarative()
 class Base:
     id: Any
     __name__: str
 
-    def __tablename__(self) -> str:
-        return self.__name__.lower()
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
