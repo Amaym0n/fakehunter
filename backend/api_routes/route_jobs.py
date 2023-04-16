51d4d66440e2db7970c243c00c5aa91a5c7e48cf
amaym0n <loco.skitls@gmail.com>
2023-04-16 20:26:05 +0300

    Added new method "retrieve_job" that retrieves job by id. If job does not exist the method returns 404

--- backend/api_routes/route_jobs.py
+++ backend/api_routes/route_jobs.py
@@ -1,10 +1,10 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from db import get_db, Jobs
-from db.repository.jobs import create_new_job
+from db.repository.jobs import create_new_job, retrieve_job_by_id
 from schemas.jobs_schema import JobCreate
 
 jobs_router = APIRouter()
@@ -14,3 +14,11 @@ jobs_router = APIRouter()
 def job_create(job: JobCreate, db: Session = Depends(dependency=get_db)) -> Jobs:
     created_job = create_new_job(job=job, db=db)
     return created_job
+
+
+@jobs_router.get(path='/job{job_id}', status_code=HTTPStatus.OK)
+def retrieve_job(job_id: int, db: Session = Depends(dependency=get_db)) -> HTTPException | Jobs:
+    retrieved_job = retrieve_job_by_id(job_id=job_id, db=db)
+    if retrieved_job is None:
+        return HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f'Job with id {job_id} does not exist')
+    return retrieved_job
