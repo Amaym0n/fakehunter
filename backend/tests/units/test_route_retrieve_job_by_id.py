9713200db307666efea499f3d57ec7930e6abf5d
amaym0n <loco.skitls@gmail.com>
2023-04-21 19:46:26 +0300

    Fixed unit test_route_retrieve_job_by_id.py

--- backend/tests/units/test_route_retrieve_job_by_id.py
+++ backend/tests/units/test_route_retrieve_job_by_id.py
@@ -10,7 +10,7 @@ from tests.urls import APIUrls
                                                  (HTTPStatus.UNPROCESSABLE_ENTITY, ''),
                                                  (HTTPStatus.UNPROCESSABLE_ENTITY, ' '),
                                                  (HTTPStatus.UNPROCESSABLE_ENTITY, -1),
-                                                 (HTTPStatus.UNPROCESSABLE_ENTITY, [1, 2, 3])])
+                                                 (HTTPStatus.NOT_FOUND, '1')])
 def test_route_retrieve_job_by_id(client, status_code, job_id):
     response = client.get(url=APIUrls.JOB.value, params={'job_id': job_id})
     assert response.status_code == status_code, \
