f567e7ef57696cccbb3c37c085536b3b4e87f50b
amaym0n <loco.skitls@gmail.com>
2023-04-23 15:46:08 +0300

    Added scope of unit tests for delete_job route

---
+++ backend/tests/units/test_route_delete_job_by_id.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+
+import pytest
+
+from tests.urls import APIUrls
+
+
+@pytest.mark.parametrize('status_code, job_id', [(HTTPStatus.NOT_FOUND, 1),
+                                                 (HTTPStatus.UNPROCESSABLE_ENTITY, 'x'),
+                                                 (HTTPStatus.UNPROCESSABLE_ENTITY, ''),
+                                                 (HTTPStatus.UNPROCESSABLE_ENTITY, ' '),
+                                                 (HTTPStatus.UNPROCESSABLE_ENTITY, -1),
+                                                 (HTTPStatus.NOT_FOUND, '1')])
+def test_route_delete_job_by_id(client, status_code, job_id):
+    response = client.delete(url=APIUrls.JOB.value, params={'job_id': job_id})
+    assert response.status_code == status_code, \
+        f'Полученный status_code {response.status_code} не соответствует ожидаемому {status_code}'
